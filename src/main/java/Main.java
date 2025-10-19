192d917465b79a34c3b2fe3222b12f52e943a751
Jonatan Rodríguez <jonodomongos@hotmail.com>
2025-10-19 21:03:07 +0200

    commit inicial

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+    /*
+    INSTRUCCIONES:
+    1. SWITCH:
+    Preguntas estación del año. Mediante un switch, respondes una cosa u otra.
+    Ejemplo "primavera". Respuesta: "la primavera me alteraaa".
+
+    2. WHILE:
+    Una asociación benéfica te envía a pedir caridad. Preguntas "una ayudita?"
+    Si te dicen que sí, preguntas cuánto quieren poner. Si te dicen que no, te deprimes y te vas para tu casa.
+    Al final, tienes que decir cuánta gente te ha ayudado y cuánto dinero has recaudado.
+
+    3. SWITCH - WHILE:
+    El archivo sonoro!
+    Usando una variable byte, preguntas cuál de los sonidos quieres escuchar.
+    Cuando eligen un número, aparece la onomatopeya de ese sonido.
+    Luego le preguntas: quieres escuchar otro? Y sigue hasta que diga no.
+
+    Ejemplo: elige 1- cohete, y suena ziuuum!, elige 3- moto, y suena brrrmmm!
+    Puedes probar la opción do...while para asegurar de que pregunta al menos una vez.
+     */
+    }
+}
